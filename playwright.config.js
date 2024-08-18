a5f64a10a3015e3930495b228c1d2ca7e5524fff
svpatron <saskapatronen@gmail.com>
2024-08-18 21:18:14 +0300

    11.9

--- playwright.config.js
+++ playwright.config.js
@@ -13,12 +13,12 @@ const { defineConfig, devices } = require('@playwright/test')
 module.exports = defineConfig({
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:8081',
+    url: 'http://127.0.0.1:8080',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: 'http://localhost:8081/',
+    baseURL: 'http://localhost:8080/',
   },
   testDir: './tests',
   /* Run tests in files in parallel */
